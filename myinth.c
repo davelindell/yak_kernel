0b9347f5e19ba8000cc1a0af9c2bfdbe72f4d72c
Nicholas Dean Bell <gizmoh@acacia.et.byu.edu>
2014-11-17 17:53:30 -0700

    Adding interrupt code

--- myinth.c
+++ myinth.c
@@ -1,51 +1,16 @@
-#include "clib.h"
 #include "yakk.h"
-#include "lab6defs.h"
-
-extern YKQ *MsgQPtr; 
-extern struct msg MsgArray[];
-extern int GlobalFlag;
+#include "lab7defs.h"
 
 extern int KeyBuffer;
-extern YKSEM *NSemPtr;
+
 
 void handleReset() {
-    exit(0);   
+    exit(0);
 }
 
 
 void handleTick() {
 	tcb_t* current;
-
-    // start lab 6 code
-    static int next = 0;
-    static int data = 0;
-    ++YKTickNum;
-
-    /* create a message with tick (sequence #) and pseudo-random data */
-    MsgArray[next].tick = YKTickNum;
-    data = (data + 89) % 100;
-    MsgArray[next].data = data;
-    if (YKQPost(MsgQPtr, (void *) &(MsgArray[next])) == 0)
-	printString("  TickISR: queue overflow! \n");
-    else if (++next >= MSGARRAYSIZE)
-	next = 0;
-    // end lab 6 code
-
-
-    printString("TICK ");
-    printInt(YKTickNum);
-    printNewLine();
-	current = YKBlockList;
-
-    /*printNewLine();
-    printString("Block List:");
-    printNewLine();
-    print_delay_list();
-    printString("Ready List:");
-    printNewLine();
-    print_ready_list();*/
-
 
 	while ( current )
 	{
@@ -61,35 +26,28 @@ void handleTick() {
 		current = current->next;
 	}
 
-    //YKSemPost(NSemPtr);
     return;
 }
 
 
-void handleKeyboard() {
-    int i = 0;
-    GlobalFlag = 1;    // for lab 6
-
-    if (KeyBuffer == 24178)
-        exit(0);
-    else if (KeyBuffer == 'd') { //^r
-        printNewLine();
-        printString("DELAY KEY PRESSED");
-        for (i = 0; i < 10000; ++i){}
-        printNewLine();
-        printString("DELAY COMPLETE");
-        printNewLine();
-    }
-    else if (KeyBuffer == 24180) { //^t
-        handleTick();
-    }
-    else {
-        printNewLine();
-        printString("KEYPRESS (");
-        printChar(KeyBuffer);
-        printString(") IGNORED");
-        printNewLine();
-    }
-    return;
-
+void handleKeyboard()
+{
+	char c = KeyBuffer;
+	switch( c )
+	{
+		case 'a': YKEventSet( charEvent, EVENT_A_KEY ); break;
+		case 'b': YKEventSet( charEvent, EVENT_B_KEY ); break;
+		case 'c': YKEventSet( charEvent, EVENT_C_KEY ); break;
+		case 'd': YKEventSet( charEvent, EVENT_A_KEY | EVENT_B_KEY | EVENT_C_KEY ); break;
+		case '1': YKEventSet( numEvent, EVENT_1_KEY ); break;
+		case '2': YKEventSet( numEvent, EVENT_2_KEY ); break;
+		case '3': YKEventSet( numEvent, EVENT_3_KEY ); break;
+		default:
+			printNewLine();
+		    printString("KEYPRESS (");
+		    printChar( c );
+		    printString(") IGNORED");
+		    printNewLine();
+			break;
+	}
 }
